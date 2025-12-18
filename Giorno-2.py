7d64fbc7ed37f4fb4962aa1c8e7315908e803217
Rick2178 <ricpat78@gmail.com>
2025-12-18 19:19:11 +0100

    Giorno 2-Le mie variabili

---
+++ Giorno-2.py
@@ -0,0 +1,13 @@
+nome="Riccardo"
+anni=47
+citta="Roma"
+lavoro="Budapest"
+Sei_impiegato=True 
+Che_tipo_di_lavoro_fai="Ho una caffetteria"
+
+print(f"Mi chiamo {nome}")
+print(f"Ho {anni} anni")
+print(f"Vivo e lavoro a {lavoro} ma sono di {citta}")
+print(f"Lavori?{Sei_impiegato}")
+print(f"Di cosa ti occupi?{Che_tipo_di_lavoro_fai}")
+
