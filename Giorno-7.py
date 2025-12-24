7c6c0f836d6417579b506acc656cb37528c2d937
Rick2178 <ricpat78@gmail.com>
2025-12-24 21:47:41 +0100

    Giorno 7 - Cicli WHILE e FOR

---
+++ Giorno-7.py
@@ -0,0 +1,20 @@
+print("="*50)
+print("CONTO alla ROVESCIA")
+print("="*50)
+
+for numero in range(5, 0, -1):
+    print(f"{numero}...")
+
+print(" VIA \n")
+
+print("=" * 50)
+print("📈 RIPETIZIONI MOTIVAZIONALI")
+print("=" * 50)
+
+contatore = 0
+
+while contatore < 3:
+    contatore = contatore + 1
+    print(f"{contatore}) Ogni giorno miglioro con Python!")
+
+
